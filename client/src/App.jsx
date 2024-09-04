655ed8e3cab3a03563c8982b2b1edca073fbd23c
Colton McCauley <cmccaulee@gmail.com>
2024-09-04 08:23:33 -0600

    Addressed merge conflicts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,6 @@ import CreateNewCampaign from "./views/CreateNewCampaign";
 import OneCampaign from "./views/OneCampaign";
 import EditCampaign from "./views/EditCampaign";
 
-
 function App() {
     return (
         <>
@@ -17,9 +16,18 @@ function App() {
                         <Route path="/" element={<Login />} />
                         <Route path="/users/register" element={<Register />} />
                         <Route path="/home" element={<Home />} />
-                        <Route path="/campaigns/create" element={<CreateNewCampaign />} />
-                        <Route path="/campaigns/:id" element={<OneCampaign />} />
-                        <Route path="/campaigns/:id/edit" element={<EditCampaign />} />
+                        <Route
+                            path="/campaigns/create"
+                            element={<CreateNewCampaign />}
+                        />
+                        <Route
+                            path="/campaigns/:id"
+                            element={<OneCampaign />}
+                        />
+                        <Route
+                            path="/campaigns/:id/edit"
+                            element={<EditCampaign />}
+                        />
                     </Routes>
                 </LoggedInUserProvider>
             </BrowserRouter>
