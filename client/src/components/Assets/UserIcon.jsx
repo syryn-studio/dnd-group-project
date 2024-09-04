63bf6d430ad151950b2456497eebc36aa41fd34e
Colton McCauley <cmccaulee@gmail.com>
2024-09-04 11:32:24 -0600

    Styling adjustments

--- client/src/components/Assets/UserIcon.jsx
+++ client/src/components/Assets/UserIcon.jsx
@@ -1,6 +1,6 @@
 const UserIcon = (props) => {
     const { image, size } = props;
-    const styling = `border border-black rounded-full shadow-sm max-w-16 max-h-16 min-w-16 min-h-${size}`;
+    const styling = `border border-black rounded-full shadow-small max-w-16 max-h-16 min-w-16 min-h-${size}`;
     return (
         <>
             <img className={styling} src={image} alt="" />
