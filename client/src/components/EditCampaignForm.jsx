1099b433d820093f067a16719b032043b2285e1a
cullenpug <145298968+cullenpug@users.noreply.github.com>
2024-09-04 12:28:06 -0500

    added user functionality to form
    Edit form added user._ID and completed inputs

--- client/src/components/EditCampaignForm.jsx
+++ client/src/components/EditCampaignForm.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from "react";
 import { redirect, useNavigate, useParams } from "react-router-dom";
 import CampaignService from "../services/campaign.services.js";
+import { useContext } from "react";
+import { LoggedInUserContext } from "../context/LoggedInUserContext.jsx";
 
 const EditCampaignForm = (props) => {
+    const { user } = useContext(LoggedInUserContext);
     const [name, setName] = useState("");
     const [description, setDescription] = useState("");
     const [image, setImage] = useState("");
@@ -34,6 +37,8 @@ const EditCampaignForm = (props) => {
     }, [id])
 
 
+
+
     const nameHandler = (e) => {
         setName(e.target.value)
     }
@@ -46,13 +51,22 @@ const EditCampaignForm = (props) => {
         setImage(e.target.value)
     }
 
+    const campaignChangeHandler = (e) => {
+        const { name, value } = e.target;
+        setCampaignData((prevCampaignData) => ({
+            ...prevCampaignData,
+            [name]: value,
+        }));
+    };
+
     const submitHandler = (e) => {
         e.preventDefault();
 
         CampaignService.updateCampaign(id, campaignData), {
             name,
             description,
-            image
+            image,
+            createdBy: user._id
         }
             .then(res => {
                 console.log(res);
@@ -69,7 +83,59 @@ const EditCampaignForm = (props) => {
 
     return (
         <>
-            <h1>This is the Edit Campaign Form</h1>
+                <div>
+                <form onSubmit={(e) => submitHandler(e)}>
+                    {errors && <p className="text-red-500">{errors.name}</p>}
+                    <div>
+                        <label htmlFor="name">Name:</label>
+                        <input
+                            type="text"
+                            name="name"
+                            value={campaignData.name}
+                            onChange={(e) => campaignChangeHandler(e)}
+                            id="name"
+                        />
+                        {errors.validationErrors && (
+                            <p className="text-red-400">
+                                {errors.validationErrors.name}
+                            </p>
+                        )}
+                    </div>
+                    <div>
+                        <label htmlFor="description">Description:</label>
+                        <input
+                            type="text"
+                            name="description"
+                            value={campaignData.description}
+                            onChange={(e) => campaignChangeHandler(e)}
+                            id="description"
+                        />
+                        {errors.validationErrors && (
+                            <p className="text-red-400">
+                                {errors.validationErrors.description}
+                            </p>
+                        )}
+                    </div>
+                    <div>
+                        <label htmlFor="image">Image:</label>
+                        <input
+                            type="text"
+                            name="image"
+                            value={campaignData.image}
+                            onChange={(e) => campaignChangeHandler(e)}
+                            id="image"
+                        />
+                        {errors.validationErrors && (
+                            <p className="text-red-400">
+                                {errors.validationErrors.image}
+                            </p>
+                        )}
+                    </div>
+                    <button className="btn btn-primary" type="submit">
+                        Make Changes
+                    </button>
+                </form>
+            </div>
         </> 
     )
 };
